2eeaae5437658b36325ba3c62f8d3520ec0f8cff
Laitei <teiteipara@gmail.com>
2026-03-06 13:06:46 -0800

    Update sitemap.xml.js

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -6,41 +6,58 @@ function formatDate(value) {
 }
 
 export async function onRequest(context) {
-  const db = context.env.DB;
 
-  const songs = await db.prepare(
-    'SELECT slug, COALESCE(updated_at, created_at) AS lastmod FROM songs WHERE slug IS NOT NULL ORDER BY id DESC'
-  ).all();
+  try {
 
-  let urls = '';
+    const db = context.env.DB;
 
-  for (const song of songs.results || []) {
-    urls += `
+    const query = await db.prepare(
+      `SELECT slug, COALESCE(updated_at, created_at) AS lastmod
+       FROM songs
+       WHERE slug IS NOT NULL
+       ORDER BY id DESC`
+    ).all();
+
+    const songs = query.results || [];
+
+    let urls = '';
+
+    for (const song of songs) {
+      urls += `
       <url>
         <loc>https://maralyrics.com/song/${song.slug}</loc>
         <lastmod>${formatDate(song.lastmod)}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.7</priority>
       </url>`;
-  }
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-    <url>
-      <loc>https://maralyrics.com/</loc>
-      <lastmod>${formatDate(new Date())}</lastmod>
-      <priority>1.0</priority>
-    </url>
-
-    ${urls}
-
-  </urlset>`;
-
-  return new Response(xml, {
-    headers: {
-      'Content-Type': 'application/xml; charset=UTF-8',
-      'Cache-Control': 'public, max-age=300',
     }
-  });
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>https://maralyrics.com/</loc>
+    <lastmod>${formatDate(new Date())}</lastmod>
+    <priority>1.0</priority>
+  </url>
+
+  ${urls}
+
+</urlset>`;
+
+    return new Response(xml, {
+      headers: {
+        "Content-Type": "application/xml",
+        "Cache-Control": "public, max-age=300"
+      }
+    });
+
+  } catch (err) {
+
+    return new Response(
+      `Sitemap generation failed: ${err.message}`,
+      { status: 500 }
+    );
+
+  }
 }
